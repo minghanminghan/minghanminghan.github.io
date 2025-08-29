f7c4d0c86b86bd993a4051f9dfcbcd565af6b7e0
minghanminghan <andrewjiang6789@gmail.com>
2025-08-29 14:55:57 -0400

    next config change

--- next.config.ts
+++ next.config.ts
@@ -1,16 +1,12 @@
 import type { NextConfig } from "next";
 
-const isProd = process.env.NODE_ENV === "production"
-const repoName = "minghanminghan.github.io"
-
 const nextConfig: NextConfig = {
   turbopack: {
     root: __dirname
   },
-  output: "export",
-  distDir: "out",
+  // output: "export",
+  // distDir: "out",
   images: { unoptimized: true },
-  basePath: isProd ? `/${repoName}` : "",
 };
 
 export default nextConfig;
