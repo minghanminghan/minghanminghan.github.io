ac6e1bae8c2c5565c26ec0b39da103cbabeee5b0
minghanminghan <andrewjiang6789@gmail.com>
2025-08-29 11:32:05 -0400

    configuring next js for gh pages

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,12 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   turbopack: {
     root: __dirname
-  }
+  },
+  output: "export",
+  distDir: "out",
+  images: { unoptimized: true },
+  basePath: "/minghanminghan.github.io",
+  assetPrefix: "/minghanminghan.github.io/",
 };
 
 export default nextConfig;
